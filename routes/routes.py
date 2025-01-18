184d0a4366a82aa2c89da529bd264c7223ba612c
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2025-01-18 22:15:12 +0530

    feat: visualisation route

--- routes/routes.py
+++ routes/routes.py
@@ -2,7 +2,15 @@ from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from utilities.database import get_db
-from routes.services import get_data, insert_data_into_db
+from routes.services import (
+    get_data,
+    insert_data_into_db,
+    visualise_sma,
+    generate_signals,
+)
+import matplotlib
+
+matplotlib.use("Agg")
 
 router = APIRouter()
 
@@ -13,5 +21,15 @@ def insert_data(db: Session = Depends(get_db)):
         data = get_data()
         insert_data_into_db(data, db)
         return JSONResponse(status_code=200, content="Data inserted successfully")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/visualise_sma")
+def visualise(short_window: int, long_window: int, forecast_steps: int):
+    try:
+        data = get_data().copy()
+        signals = generate_signals(data, short_window, long_window)
+        return visualise_sma(signals, short_window, long_window, forecast_steps)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- routes/services.py
+++ routes/services.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from models.model import Data
 from sqlalchemy.orm import Session
+import matplotlib.pyplot as plt
+from fastapi.responses import StreamingResponse
+import statsmodels.api as sm
+import io
 
 
 def get_data() -> pd.DataFrame:
@@ -20,3 +24,66 @@ def insert_data_into_db(data: pd.DataFrame, db: Session):
         )
         db.add(db_data)
     db.commit()
+
+
+def calculate_sma(data: pd.DataFrame, column: str, window: int) -> pd.Series:
+    return data[column].rolling(window=window).mean()
+
+
+def generate_signals(
+    data: pd.DataFrame, short_window: int, long_window: int
+) -> pd.DataFrame:
+    data["short_sma"] = calculate_sma(data, "close", short_window)
+    data["long_sma"] = calculate_sma(data, "close", long_window)
+    data["signal"] = 0
+    data.loc[data["short_sma"] > data["long_sma"], "signal"] = 1
+    data.loc[data["short_sma"] < data["long_sma"], "signal"] = -1
+    return data[["datetime", "close", "short_sma", "long_sma", "signal"]]
+
+
+def forecast_value(data: pd.DataFrame, forecast_steps: int) -> pd.Series:
+    model = sm.tsa.ARIMA(data["close"], order=(5, 1, 0))
+    results = model.fit()
+    forecast = results.forecast(steps=forecast_steps)
+    return forecast
+
+
+def visualise_sma(
+    data: pd.DataFrame, short_window: int, long_window: int, forecast_steps: int
+) -> StreamingResponse:
+    plt.figure(figsize=(10, 6))
+    plt.plot(data["datetime"], data["close"], label="Original Stock Price", marker="o")
+    plt.plot(
+        data["datetime"],
+        data["short_sma"],
+        label=f"Short SMA ({short_window} days)",
+        marker="o",
+        color="blue",
+    )
+    plt.plot(
+        data["datetime"],
+        data["long_sma"],
+        label=f"Long SMA ({long_window} days)",
+        marker="o",
+        color="orange",
+    )
+
+    forecast = forecast_value(data, forecast_steps)
+    forecast_index = pd.date_range(
+        start=data["datetime"].iloc[-1] + pd.Timedelta(days=1),
+        periods=forecast_steps,
+        freq="D",
+    )
+    plt.plot(forecast_index, forecast, color="red", label="Forecast")
+
+    plt.title("Simple Moving Average (SMA) Crossover with Forecast")
+    plt.ylabel("Stock Price")
+    plt.xlabel("Date")
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.grid(True)
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    plt.close()
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
