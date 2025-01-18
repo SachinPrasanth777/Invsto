d3beb5080aeab6684fe65586846987845c98472c
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2025-01-18 19:35:44 +0530

    refactor: change logic

--- routes/services.py
+++ routes/services.py
@@ -1,6 +1,22 @@
 import pandas as pd
+from models.model import Data
+from sqlalchemy.orm import Session
 
-xlsx_data = "data/HINDALCO_1D.xlsx"
 
-data = pd.read_excel(xlsx_data)
-print(data)
+def get_data() -> pd.DataFrame:
+    return pd.read_excel("data/HINDALCO_1D.xlsx")
+
+
+def insert_data_into_db(data: pd.DataFrame, db: Session):
+    for _, row in data.iterrows():
+        db_data = Data(
+            datetime=row["datetime"],
+            close=row["close"],
+            high=row["high"],
+            low=row["low"],
+            open=row["open"],
+            volume=row["volume"],
+            instrument=row["instrument"],
+        )
+        db.add(db_data)
+    db.commit()
